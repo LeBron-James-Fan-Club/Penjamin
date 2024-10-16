e71bcfc2277876e99800db151e2f75ceeba59a2b
suwa <suwuako@protonmail.com>
2024-10-17 03:03:47 +1100

    its 3am im sleeping

--- src/bootloader.s
+++ src/bootloader.s
@@ -29,7 +29,30 @@ print_boot_message__body:
     jmp     print_boot_message__cond
 
 print_boot_message__end:
-    jmp     print_boot_message__end
+    jmp     enter_a20
+
+enter_a20:
+    in      al,     0x96    ; read from the a20 port
+    or      al,     2       ; use or to flip the second last bit
+    out     0x96,   al      ; return flipped bit to enable a20
+
+gdt_start:
+gdt_start_null:
+    ; 64 bits of 0s, buffer for cpu to use
+    dq      0
+gdt_segment:
+gdt_segment_base:
+    ; 32 bit value
+    ; earliest memory address you can find this segment at
+    dw      0
+gdt_segment_limit:
+    ; 20 bit value (relic of a20 mode)
+    ; maximum address (contrary to base address)
+    db 
+gdt_segment_access:
+gdt_segment_flags:
+    
+
 
 message_on_boot:
     db "sigma sigma", 0
